46d4a2954a057944f06e62f361dfc5f229982cc3
unknown <stephanielakin13@outlook.com>
2022-09-06 20:38:09 -0400

    adding initial pages and migration for DB creation

---
+++ BookListRazor/Migrations/20220906205921_UpdateBook.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BookListRazor.Migrations
+{
+    public partial class UpdateBook : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Book");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MyProperty",
+                table: "Book",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
